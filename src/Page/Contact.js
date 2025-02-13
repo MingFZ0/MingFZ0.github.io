3d4f6867649b7a7e6792b3534c8c54dc8347da4a
MingFZ0 <mingfengz1678@gmail.com>
2025-02-13 02:23:40 -0500

    Resume got moved to public folder

--- src/Page/Contact.js
+++ src/Page/Contact.js
@@ -23,7 +23,7 @@ function Contact() {
                     <p>GitHub</p>
                 </Col>
                 <Col sm="3">
-                    <a href="./static/media/Resume.html" target="_blank" type="application/pdf">
+                    <a href="./resume/Resume.html" target="_blank" type="application/pdf">
                         <img alt="Resume PDF Icon" src={PDFImg} width={40} height={40}/>
                     </a>
                     <p>Resume</p>
