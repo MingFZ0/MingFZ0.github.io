208095db051d9faa02a79e9737484a1271d115eb
MingFZ0 <mingfengz1678@gmail.com>
2025-02-15 17:08:58 -0500

    Project section now uses flex

--- src/Page/components/ScrollSlide/GallaryCard.js
+++ src/Page/components/ScrollSlide/GallaryCard.js
@@ -17,56 +17,47 @@ function GallaryCard(props) {
     let url = props.url;
     if (url != null)
         return (
-            <Container className='GallaryCard' key={Math.random()}>
-                <Row>
-                    <Col sm="1">
+            <div className='GallaryCard' key={Math.random()}>
+                <div className='GallaryCardContainer'>
                     <button className='GallaryCardButton' onClickCapture={() => 
                         props.onIndexChange(prevIndex, "AnimeSlideLeft")}>
                             <img src={LeftArr} width={50} height={50}></img>
                         </button>
-                    </Col>
-                    <Col sm={4} className={'GallaryCardImg ' + slideDir}>
+                    <div className={'GallaryCardImg ' + slideDir}>
                         <img src={img} alt={imgAlt} width={"100%"} height={"300px"}/>
-                        </Col>
-                    <Col className={'GallaryCardDescription '+ slideDir}>
+                        </div>
+                    <div className={'GallaryCardDescription '+ slideDir}>
                         <p>{text}</p>
                         <a href={url}>Visit Site</a>
-                        </Col>
-                    <Col sm="1">
-                        <button className='GallaryCardButton' onClickCapture={() => {
-                            props.onIndexChange(nextIndex, "AnimeSlideRight");}}>
-                            <img src={RightArr} width={50} height={50}></img>
-                        </button>
-                    </Col>
-                </Row>
-                
-            </Container>
+                        </div>
+                    <button className='GallaryCardButton' onClickCapture={() => {
+                        props.onIndexChange(nextIndex, "AnimeSlideRight");}}>
+                        <img src={RightArr} width={50} height={50}></img>
+                    </button>
+                </div>
+            </div>
     )
     else 
         return (
-            <Container className='GallaryCard' key={Math.random()}>
-                <Row>
-                    <Col sm="1">
+            <div className='GallaryCard' key={Math.random()}>
+                <div className='GallaryCardContainer'>
                     <button className='GallaryCardButton' onClickCapture={() => 
                         props.onIndexChange(prevIndex, "AnimeSlideLeft")}>
                             <img src={LeftArr} width={50} height={50}></img>
                         </button>
-                    </Col>
-                    <Col sm={4} className={'GallaryCardImg ' + slideDir}>
+                    <div className={'GallaryCardImg ' + slideDir}>
                         <img src={img} alt={imgAlt} width={"100%"} height={"300px"}/>
-                        </Col>
-                    <Col className={'GallaryCardDescription '+ slideDir}>
+                        </div>
+                    <div className={'GallaryCardDescription '+ slideDir}>
                         <p>{text}</p>
-                        </Col>
-                    <Col sm="1">
-                        <button className='GallaryCardButton' onClickCapture={() => {
-                            props.onIndexChange(nextIndex, "AnimeSlideRight");}}>
-                            <img src={RightArr} width={50} height={50}></img>
+                        </div>
+                    <button className='GallaryCardButton' onClickCapture={() => {
+                        props.onIndexChange(nextIndex, "AnimeSlideRight");}}>
+                        <img src={RightArr} width={50} height={50}></img>
                         </button>
-                    </Col>
-                </Row>
+                </div>
                 
-            </Container>
+            </div>
     )
 }
 
