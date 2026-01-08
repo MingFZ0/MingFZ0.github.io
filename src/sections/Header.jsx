5e21704c572f30bb65cceabdb56d5b15cf196fb5
Mingfeng Zhong <mingfengz1678@gmail.com>
2026-01-08 15:30:49 -0500

    Added the Header Descriptor

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -3,13 +3,16 @@ import "./Header.css";
 
 function Header() {
     return(
-        <div className="Header">
-            <NavControl text="About" link="#Top"/>
-            <NavControl text="Technology" link="#Technology"/>
-            <h2 color="light" id="Header-Title">Mingfeng Zhong</h2>
-            <NavControl text="Details" link="#Details"/>
-            <NavControl text="Contact" link="#Contact"/>
-        </div>
+        <>
+            <div className="Header">
+                <NavControl text="About" link="#Top"/>
+                <NavControl text="Technology" link="#Technology"/>
+                <h2 color="light" id="Header-Title">Mingfeng Zhong</h2>
+                <NavControl text="Details" link="#Details"/>
+                <NavControl text="Contact" link="#Contact"/>
+            </div>
+            <h3 id="Header-Descriptor">Aim to Move Hearts</h3>
+        </>
     )
 
 }
