ae48000fe1b2493e68bcd2b52183e77d215551e0
Mingfeng Zhong <mingfengz1678@gmail.com>
2026-01-09 16:14:27 -0500

    Tech scrollbar is done

--- src/sections/utility/Carousel.jsx
+++ src/sections/utility/Carousel.jsx
@@ -2,9 +2,33 @@ import ItemPanel from "./ItemPanel";
 import "./Carousel.css";
 
 import ReactIcon from "../../assets/media/icons/ReactIcon.png";
+import { useEffect, useState } from "react";
+import { tr } from "motion/react-client";
 
 
 function Carousel(props) {
+
+    // const [active, setActive] = useState(false);
+    // const [scrollPos, setScrollPos] = useState(0);
+    // const [prevPos, setPrevPos] = useState(0);
+
+    // const handleSelect = (e) => {
+
+    //     if (active == true) {
+    //         setActive(false);
+    //         return;
+    //     }
+    //     const y = window.scrollY;
+    //     setPrevPos(y);
+    //     setActive(true);
+    //     console.log("Pos: " + y);
+        
+    //     let myScroll = document.querySelector(".Carousel-Container");
+    //     myScroll.addEventListener("wheel", (e) => {
+    //         e.preventDefault();
+    //         myScroll.scrollLeft += (e.deltaY * 2);
+    //     });
+    // }
 
     const items = props.items;
 
