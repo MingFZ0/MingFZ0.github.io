acc969b4db995c3b3a4080ecf9d9ade6becf13cb
Mingfeng Zhong <mingfengz1678@gmail.com>
2026-01-09 18:35:14 -0500

    Stumbing

--- src/sections/Details.jsx
+++ src/sections/Details.jsx
@@ -1,7 +1,18 @@
 function Details() {
+
+
+
     return( 
-        <>
-        </>
+        <div className="Details">
+            <div id="Details-Sections">
+                <div id="Details-Sections-Previous"></div>
+                <div id="Details-Sections-Current"></div>
+                <div id="Details-Sections-Next"></div>
+            </div>
+            <div id="Details-Cards">
+                
+            </div>
+        </div>
     )
 }
 
