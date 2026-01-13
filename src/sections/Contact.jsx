bd835d0ccf2f1c3c36b667371e4ceba7dfb5c298
Mingfeng Zhong <mingfengz1678@gmail.com>
2026-01-13 16:24:54 -0500

    dialogue is done

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,7 +1,35 @@
+import "./Contact.css";
+import pfp from "../assets/media/pfp.png";
+import mail from "../assets/media/icons/mail.png";
+
 function Contact() {
     return (
-        <>
-        </>
+        <div className="Contact">
+            <h2 id="Contact-Title">Contact</h2>
+            <div className="Contact-BorderDiv" />
+            <div className="Contact-MessageGroup">
+                <img className="Contact-MessageGroup-pfp" src={pfp} />
+                <div className="Contact-MessageGroup-Message">
+                    <p className="Contact-MessageGroup-text">Send me a message!</p>
+                    <a className="Contact-MessageGroup-email" href="mailto:mingfengz1678@gmail.com">
+                        <img src={mail} />
+                        <p>mingfengz1678@gmail.com</p>
+                    </a>
+                </div>
+                
+            </div>
+            <div className="Contact-Form">
+                <div className="Contact-Form-Display">
+                    <div className="">
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+                <img></img>
+            </div>
+        </div>
     )
 }
 
